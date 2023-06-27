17a78ad36467d6e1461ca9eaca7a34792aa08657
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2023-06-27 00:43:44 -0400

    initial create + skeleton bc I forgot to commit

---
+++ BlogCard/src/App.tsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+
+function App() {
+
+  return (
+    <>
+      <section>
+        <div>
+          <img />
+        </div>
+        <div>
+          <p className="bg-orange-500 rounded-xl text-white w-[130px] text-center">TECHNOLOGY</p>
+          <h3 className="">How I Built Web Development Challenges Website</h3>
+          <p>If you're looking for inspiration to build your own side project...</p>
+        </div>
+        <figure>
+          <img />
+          <figcaption>
+            <h4 className="font-bold">Corey Lubin</h4>
+            <p className="text-slate-500">4 h ago</p>
+          </figcaption>
+        </figure>
+      </section>
+    </>
+
+  )
+}
+
+export default App
