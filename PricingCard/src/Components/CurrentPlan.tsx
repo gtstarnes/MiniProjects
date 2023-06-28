a825b102b55f4e234c16664346abbd8b6fa126cd
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2023-06-27 23:11:02 -0400

    finished currentplan component

--- PricingCard/src/Components/CurrentPlan.tsx
+++ PricingCard/src/Components/CurrentPlan.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const CurrentPlan = () => {
   return (
-    <header className="text-white bg-red-600 flex flex-col gap-4 p-4 rounded-b-xl w-full h-[100px]">
+    <header className="text-white bg-red-600 flex flex-col gap-2 p-2 rounded-b-xl w-full h-[120px] items-center justify-center">
         <p>Your current plan:</p>
-        <h3 className="font-bold">Starter Trial * 500MAUs</h3>
+        <h3 className="font-bold text-xl">Starter Trial • 500MAUs</h3>
     </header>
   )
 }
