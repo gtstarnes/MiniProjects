b384b5b684a8e84ac037cd5b1035235512ff86e0
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2023-06-30 01:01:45 -0400

    added toggle button

---
+++ PricingCard/src/Components/Toggle.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { useState } from 'react'
+
+const Toggle = () => {
+    const [toggle, setToggle] = useState(true)
+
+    const handleToggle = () => {
+        setToggle(!toggle)
+    }
+
+  return (
+    <div>        
+    <label className="relative inline-flex items-center gap-2 ">
+        <span>Billed monthly</span>
+        <input type="checkbox" onClick={handleToggle} className="sr-only peer " checked={toggle} />
+        <div className={`toggle__line w-12 h-6 rounded-full ${toggle ? 'bg-red-500' : 'bg-[#202020]'} transition-colors duration-300` }></div>
+        <div className={`toggle__dot absolute w-6 h-6 bg-white rounded-full inset-y-0 left-[110px] cursor-pointer" ${toggle ? 'translate-x-6' : 'translate-x-0'} transition-transform duration-300`}></div>
+        <span>Billed annually</span>
+    </label>
+
+    </div>
+  )
+}
+
+export default Toggle
--- PricingCard/src/Pages/Plans.tsx
+++ PricingCard/src/Pages/Plans.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
+import Toggle from '../Components/Toggle';
 import PriceCard from '../Components/PriceCard';
 
 const Plans = () => {
   return ( 
     <main className="h-full w-full flex flex-col items-center pt-10 gap-6 pb-40">
         <h1 className="font-bold text-3xl">Choose a plan</h1>
-        <label>
-            <span>Billed monthly</span>
-            <input type="checkbox" value="" className="sr-only peer" checked></input>
-            <span>Billed annually</span>
-        </label>
+        <Toggle />
         <PriceCard />
     </main>
   )
