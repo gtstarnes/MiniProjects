09f85392621e4c2120f42c94317de600247936aa
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2023-06-30 01:23:27 -0400

    updated styles; incomplete price update toggle

--- PricingCard/src/Components/PriceCard.tsx
+++ PricingCard/src/Components/PriceCard.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-
+import { useState } from 'react'
 
 const plans = [
     {
       id: 1,
       plan: "Starter",
       price: "$19",
+      annual: "$209",
       features: [
         "500MAUs", "3 Projects", "Unlimited guides", "Unlimited flows", "Unlimited branded themes", "Email support"
       ]
@@ -14,6 +15,7 @@ const plans = [
       id: 2,
       plan: "Pro",
       price: "$99",
+      annual: "$1089",
       features: [
         "2500MAUs", "All starter features plus:", "Unlimited projects", "Unlimited fully customizable themes", "A dedicated Customer Success Manager"
       ]
@@ -29,15 +31,17 @@ const plans = [
   ]
 
 const PriceCardItem = (props: any) => {
+  const [annual, setAnnual] = useState(true);
+
   return (
     <article className="bg-[#202020] w-[100%] flex flex-col items-center gap-6 px-8 pt-8 rounded-md h-[600px] max-w-[500px]">
         <h4 className="text-xl">{props.card.plan}</h4>
-        <h1 className="text-5xl font-bold">{props.card.price}</h1>
+        {!annual ? <h1 className="text-5xl font-bold">{props.card.price}</h1> : <h1 className="text-5xl font-bold">{props.card.annual}</h1>}
         <ul className="list-disc marker:text-red-500 ">{props.card.features.map((feature: string) => {
             return <li className="mt-4">{feature}</li>
         })}
         </ul>
-        <button className={props.card.plan === "Pro" ? "bg-red-500 w-60 h-16 rounded-sm mt-[120px] active:bg-white active:text-red-500" : "w-60 h-16 rounded-sm mt-20 active:bg-red-500 border border-red-500"}>Choose Plan</button>
+        <button className={props.card.plan === "Pro" ? "bg-red-500 w-60 h-16 rounded-sm mt-[120px] active:bg-red-700" : props.card.plan === "Enterprise" ?  "w-60 h-16 rounded-sm mt-[130px] active:bg-red-500 border border-red-500" : "w-60 h-16 rounded-sm mt-20 active:bg-red-500 border border-red-500"}>Choose Plan</button>
     </article>
   )
 }
