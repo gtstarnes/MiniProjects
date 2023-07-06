7af5bd2f1ba255693d2aa37e73697bdb94f6e413
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2023-07-06 14:55:26 -0400

    added buttons for navigation

--- basicReactRouter/src/App.tsx
+++ basicReactRouter/src/App.tsx
@@ -1,13 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
+      <Link to="/"><button type="button">Profile</button></Link>
       <h1>Hello from App</h1>
     </>
   )
--- basicReactRouter/src/Profile.tsx
+++ basicReactRouter/src/Profile.tsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Profile = () => {
   return (
     <div>
+        <Link to="/"><button type="button">Home</button></Link>
         <h1>Hello From Profile</h1>
     </div>
   )
