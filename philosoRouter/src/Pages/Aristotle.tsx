053d514292716bac8fa3d7b587e903464398df23
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2023-07-11 01:07:24 -0400

    added navs to pages

--- philosoRouter/src/Pages/Aristotle.tsx
+++ philosoRouter/src/Pages/Aristotle.tsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Aristotle = () => {
   return (
-    <div>Aristotle</div>
+    <div>
+        <p>Aristotle</p>
+        <nav>
+            <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/socrates">Socrates</Link></li>
+            <li><Link to="/plato">Plato</Link></li>
+            <li><Link to="/aristotle">Aristotle</Link></li>
+            </ul>
+        </nav>
+    </div>
   )
 }
 
--- philosoRouter/src/Pages/Socrates.tsx
+++ philosoRouter/src/Pages/Socrates.tsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Socrates = () => {
   return (
-    <div>Socrates</div>
+    <div>
+        <p>Socrates</p>
+        <nav>
+            <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/socrates">Socrates</Link></li>
+            <li><Link to="/plato">Plato</Link></li>
+            <li><Link to="/aristotle">Aristotle</Link></li>
+            </ul>
+        </nav>
+    </div>
   )
 }
 
