602ed311bdd6d6d92ce95a7407421fe8cf230381
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2023-07-11 01:04:22 -0400

    added routes to RouteSwitch

--- philosoRouter/src/RouteSwitch.tsx
+++ philosoRouter/src/RouteSwitch.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
+import Plato from "./Pages/Plato";
+import Socrates from "./Pages/Socrates";
+import Aristotle from "./Pages/Aristotle";
 
 
 const RouteSwitch = () => {
@@ -8,7 +11,9 @@ const RouteSwitch = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
-
+        <Route path="/socrates" element={<Socrates />} />
+        <Route path="/Plato" element={<Plato />} />
+        <Route path="/Aristotle" element={<Aristotle />} />
       </Routes>
     </BrowserRouter>
   );
