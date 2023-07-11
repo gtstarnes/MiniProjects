c9eabe63a506a81e97aa74320507dab71c27fc4a
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2023-07-11 01:01:26 -0400

    added inital navs to home page

--- philosoRouter/src/App.tsx
+++ philosoRouter/src/App.tsx
@@ -1,9 +1,17 @@
-
+import { Link } from "react-router-dom";
 
 function App() {
   return (
     <>
       <p className="font-bold">Hello World</p>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/socrates">Socrates</Link></li>
+          <li><Link to="/plato">Plato</Link></li>
+          <li><Link to="/aristotle">Aristotle</Link></li>
+        </ul>
+      </nav>
     </>
   )
 }
