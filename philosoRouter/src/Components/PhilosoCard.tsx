4f2ff62c6d3e2a1fc27de25c9f8c8e7195e54012
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2023-07-11 01:28:49 -0400

    created the basic card layout

--- philosoRouter/src/Components/PhilosoCard.tsx
+++ philosoRouter/src/Components/PhilosoCard.tsx
@@ -3,14 +3,25 @@ import React from 'react';
 interface PhilosoCardProps {
     name: string;
     date: string;
-    image: HTMLImageElement;
     life: string;
     ideas: string;
 }
 
-const PhilosoCard = () => {
+const PhilosoCard = ({name, date, life, ideas}: PhilosoCardProps) => {
   return (
-    <div>PhilosoCard</div>
+    <div className="card">
+        <h1>{name}</h1>
+        <h3>{date}</h3>
+        <img></img>
+        <section>
+            <h2>Life and Times</h2>
+            <ul>{life}</ul>
+        </section>
+        <section>
+            <h2>Main Contributions to Philosophy</h2>
+            <ul>{ideas}</ul>
+        </section>
+    </div>
   )
 }
 
