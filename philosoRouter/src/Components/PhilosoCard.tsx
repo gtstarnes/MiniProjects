86078980baa9f059f50c0dd724c13369d92b324a
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2023-07-11 01:42:33 -0400

    added list item mapping

--- philosoRouter/src/Components/PhilosoCard.tsx
+++ philosoRouter/src/Components/PhilosoCard.tsx
@@ -3,8 +3,8 @@ import React from 'react';
 interface PhilosoCardProps {
     name: string;
     date: string;
-    life: string;
-    ideas: string;
+    life: string[];
+    ideas: string[];
 }
 
 const PhilosoCard = ({name, date, life, ideas}: PhilosoCardProps) => {
@@ -14,12 +14,20 @@ const PhilosoCard = ({name, date, life, ideas}: PhilosoCardProps) => {
         <h3>{date}</h3>
         <img></img>
         <section>
-            <h2>Life and Times</h2>
-            <ul>{life}</ul>
+            <h2>Main Contributions to Philosophy</h2>
+            <ul>
+                {ideas.map((idea, id) => (
+                    <li key={id}>{idea}</li>
+                ))}
+            </ul>
         </section>
         <section>
-            <h2>Main Contributions to Philosophy</h2>
-            <ul>{ideas}</ul>
+            <h2>Life and Times</h2>
+            <ul>
+                {life.map((event, id) => (
+                    <li key={id}>{event}</li>
+                ))}
+            </ul>
         </section>
     </div>
   )
