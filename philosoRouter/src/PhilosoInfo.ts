05b32206f6b291de187069c0273175f6d800f6b9
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2023-07-12 00:12:53 -0400

    added object info for cards

---
+++ philosoRouter/src/PhilosoInfo.ts
@@ -0,0 +1,86 @@
+export const socrates = {
+    name: "Socrates",
+    date: "470 BCE- 399 BCE",
+    life: [ 
+     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+     "Convallis aenean et tortor at risus viverra adipiscing at.",
+     "Facilisis volutpat est velit egestas dui id ornare.",
+     "Mi ipsum faucibus vitae aliquet nec ullamcorper sit amet.",
+     "Egestas dui id ornare arcu odio ut.",
+     "Morbi tristique senectus et netus.",
+     "Eu scelerisque felis imperdiet proin fermentum leo vel orci.",
+     "Cursus euismod quis viverra nibh cras pulvinar.",
+     "Elementum pulvinar etiam non quam lacus suspendisse faucibus.",
+     "Turpis cursus in hac habitasse platea."
+    ],
+    ideas:  [ 
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    "Convallis aenean et tortor at risus viverra adipiscing at.",
+    "Facilisis volutpat est velit egestas dui id ornare.",
+    "Mi ipsum faucibus vitae aliquet nec ullamcorper sit amet.",
+    "Egestas dui id ornare arcu odio ut.",
+    "Morbi tristique senectus et netus.",
+    "Eu scelerisque felis imperdiet proin fermentum leo vel orci.",
+    "Cursus euismod quis viverra nibh cras pulvinar.",
+    "Elementum pulvinar etiam non quam lacus suspendisse faucibus.",
+    "Turpis cursus in hac habitasse platea."
+    ]
+}
+
+export const plato = {
+    name: "Plato",
+    date: "428 BCE- 348 BCE",
+    life: [ 
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        "Convallis aenean et tortor at risus viverra adipiscing at.",
+        "Facilisis volutpat est velit egestas dui id ornare.",
+        "Mi ipsum faucibus vitae aliquet nec ullamcorper sit amet.",
+        "Egestas dui id ornare arcu odio ut.",
+        "Morbi tristique senectus et netus.",
+        "Eu scelerisque felis imperdiet proin fermentum leo vel orci.",
+        "Cursus euismod quis viverra nibh cras pulvinar.",
+        "Elementum pulvinar etiam non quam lacus suspendisse faucibus.",
+        "Turpis cursus in hac habitasse platea."
+       ],
+       ideas:  [ 
+       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+       "Convallis aenean et tortor at risus viverra adipiscing at.",
+       "Facilisis volutpat est velit egestas dui id ornare.",
+       "Mi ipsum faucibus vitae aliquet nec ullamcorper sit amet.",
+       "Egestas dui id ornare arcu odio ut.",
+       "Morbi tristique senectus et netus.",
+       "Eu scelerisque felis imperdiet proin fermentum leo vel orci.",
+       "Cursus euismod quis viverra nibh cras pulvinar.",
+       "Elementum pulvinar etiam non quam lacus suspendisse faucibus.",
+       "Turpis cursus in hac habitasse platea."
+       ]
+}
+
+export const aristotle = {
+    name: "Aristotle",
+    date: "384 BCE- 322 BCE",
+    life: [ 
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        "Convallis aenean et tortor at risus viverra adipiscing at.",
+        "Facilisis volutpat est velit egestas dui id ornare.",
+        "Mi ipsum faucibus vitae aliquet nec ullamcorper sit amet.",
+        "Egestas dui id ornare arcu odio ut.",
+        "Morbi tristique senectus et netus.",
+        "Eu scelerisque felis imperdiet proin fermentum leo vel orci.",
+        "Cursus euismod quis viverra nibh cras pulvinar.",
+        "Elementum pulvinar etiam non quam lacus suspendisse faucibus.",
+        "Turpis cursus in hac habitasse platea."
+       ],
+       ideas:  [ 
+       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+       "Convallis aenean et tortor at risus viverra adipiscing at.",
+       "Facilisis volutpat est velit egestas dui id ornare.",
+       "Mi ipsum faucibus vitae aliquet nec ullamcorper sit amet.",
+       "Egestas dui id ornare arcu odio ut.",
+       "Morbi tristique senectus et netus.",
+       "Eu scelerisque felis imperdiet proin fermentum leo vel orci.",
+       "Cursus euismod quis viverra nibh cras pulvinar.",
+       "Elementum pulvinar etiam non quam lacus suspendisse faucibus.",
+       "Turpis cursus in hac habitasse platea."
+       ]
+}
