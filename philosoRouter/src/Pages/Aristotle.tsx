e1300808570570d38da601e47e3c632a1af05bcd
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2023-07-12 00:16:35 -0400

    added info and card to aristotle page

--- philosoRouter/src/Pages/Aristotle.tsx
+++ philosoRouter/src/Pages/Aristotle.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { aristotle } from '../PhilosoInfo';
+import PhilosoCard from '../Components/PhilosoCard';
 
 const Aristotle = () => {
   return (
-    <div>
-        <p>Aristotle</p>
+    <div> 
         <nav>
             <ul>
             <li><Link to="/">Home</Link></li>
@@ -13,6 +14,7 @@ const Aristotle = () => {
             <li><Link to="/aristotle">Aristotle</Link></li>
             </ul>
         </nav>
+        <PhilosoCard name={aristotle.name} date={aristotle.date} life={aristotle.life} ideas={aristotle.ideas} />
     </div>
   )
 }
