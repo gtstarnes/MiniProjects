be1ea8fec23711abee6faff1399c7487de7d5ff1
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2023-07-12 01:02:09 -0400

    added basic styles to philosoCard

--- philosoRouter/src/Components/PhilosoCard.tsx
+++ philosoRouter/src/Components/PhilosoCard.tsx
@@ -9,21 +9,21 @@ interface PhilosoCardProps {
 
 const PhilosoCard = ({name, date, life, ideas}: PhilosoCardProps) => {
   return (
-    <div className="card">
-        <h1>{name}</h1>
-        <h3>{date}</h3>
+    <div className="card flex flex-col gap-4 w-[90%] mx-auto bg-[#F9F6F0] rounded-md p-8 shadow-md">
+        <h1 className="self-center text-6xl">{name}</h1>
+        <h3 className="self-center mt-[-10px]">{date}</h3>
         <img></img>
         <section>
-            <h2>Main Contributions to Philosophy</h2>
-            <ul>
+            <h2 className="text-lg font-bold">Main Contributions to Philosophy</h2>
+            <ul className="list-disc ml-8">
                 {ideas.map((idea, id) => (
                     <li key={id}>{idea}</li>
                 ))}
             </ul>
         </section>
         <section>
-            <h2>Life and Times</h2>
-            <ul>
+            <h2 className="text-lg font-bold">Life and Times</h2>
+            <ul className="list-disc ml-8">
                 {life.map((event, id) => (
                     <li key={id}>{event}</li>
                 ))}
