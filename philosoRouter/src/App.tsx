b2f3063ec1a470f215b6cab0035aa99d053cfcde
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2023-07-12 00:42:40 -0400

    set up basic styles for mobile

--- philosoRouter/src/App.tsx
+++ philosoRouter/src/App.tsx
@@ -2,16 +2,16 @@ import { Link } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <p className="font-bold">Hello World</p>
-      <nav>
-        <ul>
+    <div>
+      <h1 className="absolute text-6xl top-20 font-bold w-full text-center">PhiloCARDS</h1>
+      <nav className="flex h-screen items-center justify-center w-full border">
+        <ul className="flex align-middle gap-10">
           <li><Link to="/socrates">Socrates</Link></li>
           <li><Link to="/plato">Plato</Link></li>
           <li><Link to="/aristotle">Aristotle</Link></li>
         </ul>
       </nav>
-    </>
+    </div>
   )
 }
 
