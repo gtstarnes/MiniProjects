8df2fab6228a54b568e434f6efd632dc2b3b4c37
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2023-07-12 00:24:53 -0400

    added info/card, removed linked to plato

--- philosoRouter/src/Pages/Plato.tsx
+++ philosoRouter/src/Pages/Plato.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { plato } from '../PhilosoInfo';
+import PhilosoCard from '../Components/PhilosoCard';
 
 const Plato = () => {
   return (
     <div>
-        <p>Plato</p>
         <nav>
             <ul>
             <li><Link to="/">Home</Link></li>
             <li><Link to="/socrates">Socrates</Link></li>
-            <li><Link to="/plato">Plato</Link></li>
             <li><Link to="/aristotle">Aristotle</Link></li>
             </ul>
         </nav>
+        <PhilosoCard name={plato.name} date={plato.date} life={plato.life} ideas={plato.ideas} />
     </div>
   )
 }
