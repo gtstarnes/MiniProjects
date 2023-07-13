060f3228ddae03d008bb4361c9a3fdfcd1ae9cb9
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2023-07-13 03:19:48 -0400

    added summary to interface and card

--- philosoRouter/src/Components/PhilosoCard.tsx
+++ philosoRouter/src/Components/PhilosoCard.tsx
@@ -3,16 +3,21 @@ import React from 'react';
 interface PhilosoCardProps {
     name: string;
     date: string;
+    summary: string;
     life: string[];
     ideas: string[];
 }
 
-const PhilosoCard = ({name, date, life, ideas}: PhilosoCardProps) => {
+const PhilosoCard = ({name, date, summary, life, ideas}: PhilosoCardProps) => {
   return (
     <div className="card flex flex-col gap-4 w-[90%] mx-auto bg-[#F9F6F0] rounded-md p-8 shadow-md">
         <h1 className="self-center text-6xl">{name}</h1>
         <h3 className="self-center mt-[-10px]">{date}</h3>
         <img></img>
+        <section>
+            <h2>Summary</h2>
+            <p>{summary}</p>
+        </section>
         <section>
             <h2 className="text-lg font-bold">Main Contributions to Philosophy</h2>
             <ul className="list-disc ml-8">
