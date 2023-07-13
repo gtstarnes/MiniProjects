1111a347c34cfba738cf64b8ccf8e4ff46d7189f
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2023-07-13 03:20:25 -0400

    added fetch for summary from wiki api

--- philosoRouter/src/PhilosoInfo.ts
+++ philosoRouter/src/PhilosoInfo.ts
@@ -1,6 +1,46 @@
+async function fetchData(url:string): Promise<string> {
+  
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error('Failed to fetch API data');
+    }
+    const data = await response.json();
+    return data.extract;
+  }
+  
+  // Usage
+const socratesData = await fetchData('https://en.wikipedia.org/api/rest_v1/page/summary/socrates')
+    .then(extract => {
+      return extract; // Use the extracted value as needed
+    })
+    .catch(error => {
+      console.error(error);
+    });
+
+const platoData = await fetchData('https://en.wikipedia.org/api/rest_v1/page/summary/plato')
+.then(extract => {
+    return extract; // Use the extracted value as needed
+})
+.catch(error => {
+    console.error(error);
+});
+
+const aristotleData = await fetchData('https://en.wikipedia.org/api/rest_v1/page/summary/aristotle')
+.then(extract => {
+    return extract; // Use the extracted value as needed
+})
+.catch(error => {
+    console.error(error);
+});
+
+
+
+    
+
 export const socrates = {
     name: "Socrates",
     date: "470 BCE- 399 BCE",
+    summary: socratesData,
     life: [ 
      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
      "Convallis aenean et tortor at risus viverra adipiscing at.",
@@ -30,6 +70,7 @@ export const socrates = {
 export const plato = {
     name: "Plato",
     date: "428 BCE- 348 BCE",
+    summary: platoData,
     life: [ 
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
         "Convallis aenean et tortor at risus viverra adipiscing at.",
@@ -59,6 +100,7 @@ export const plato = {
 export const aristotle = {
     name: "Aristotle",
     date: "384 BCE- 322 BCE",
+    summary: aristotleData,
     life: [ 
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
         "Convallis aenean et tortor at risus viverra adipiscing at.",
